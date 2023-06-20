518cd751970fd76b98cf764738a72f2ee4018fe3
spaceymonk <b.kaan.ozkan@gmail.com>
2023-06-20 23:39:48 +0300

    feat: add about modal

--- app/home-components/button-group.tsx
+++ app/home-components/button-group.tsx
@@ -1,23 +1,75 @@
 // TODO: write generate button logic
 // TODO: write about button logic
 
+import "./about-modal.css";
+
+import { useState } from "react";
+import Modal from "react-modal";
+
 export default function ButtonGroup() {
+  const [showModal, setShowModal] = useState(false);
+
   return (
-    <div className="my-4 mx-6 select-none">
-      <button
-        className="bg-interact text-white capitalize border-2 border-interact rounded-xl
-                  transition duration-150 ease-in-out px-6 py-1 mr-4 
-                  hover:contrast-50"
+    <>
+      <div className="my-4 mx-6 select-none">
+        <button
+          className="bg-interact text-white capitalize border-2 border-interact rounded-xl
+                    transition duration-150 ease-in-out px-6 py-1 mr-4 
+                    hover:contrast-50"
+        >
+          generate
+        </button>
+        <button
+          onClick={() => setShowModal(true)}
+          className="bg-app text-interact capitalize border-2 border-interact rounded-xl
+                    transition duration-150 ease-in-out px-6 py-1
+                    hover:bg-interact hover:text-white hover:contrast-50"
+        >
+          about
+        </button>
+      </div>
+      <Modal
+        isOpen={showModal}
+        onRequestClose={() => setShowModal(false)}
+        closeTimeoutMS={150}
+        className="bg-app text-info font-sans rounded-2xl text-center mx-8 w-fit  md:w-96 p-5 drop-shadow-lg select-none"
       >
-        generate
-      </button>
-      <button
-        className="bg-app text-interact capitalize border-2 border-interact rounded-xl
-                  transition duration-150 ease-in-out px-6 py-1
-                  hover:bg-interact hover:text-white hover:contrast-50"
-      >
-        about
-      </button>
-    </div>
+        <h2 className="text-info text-center font-bold text-xl underline uppercase font-sans mb-5">
+          about
+        </h2>
+        <div className="mt-5">
+          <p className="my-3">
+            Create your automation workflow by creating and connecting the
+            blocks.
+          </p>
+          <p className="my-3">
+            Click “Generate” button to get the Python script of the automation.
+          </p>
+          <p className="my-3">
+            To run the generated script you will need to setup a Python
+            environment.
+          </p>
+        </div>
+        <p className="italic my-5">
+          More details on GitHub:
+          <br />
+          <a
+            href="https://www.github.com/spaceymonk/automation-script-generator"
+            className="text-interact underline block hover:contrast-75"
+          >
+            github.com/spaceymonk/automation-script-generator
+          </a>
+        </p>
+
+        <button
+          onClick={() => setShowModal(false)}
+          className="bg-app text-interact capitalize border-2 border-interact rounded-xl
+                    transition duration-150 ease-in-out px-6 py-1
+                    hover:bg-interact hover:text-white hover:contrast-50"
+        >
+          close
+        </button>
+      </Modal>
+    </>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 'use client'
 import 'reactflow/dist/style.css';
 
+import Modal from 'react-modal';
+
 import Board from "./home-components/board";
 import ButtonGroup from "./home-components/button-group";
 import Title from "./home-components/title";
 
 export default function Home() {
+  Modal.setAppElement('#root');
   return (
     <div className="bg-app flex flex-col h-screen">
       <Title />
