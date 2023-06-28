9fc427d113bf235abcdc03ef92aaa7c6e9d4b031
spaceymonk <b.kaan.ozkan@gmail.com>
2023-06-28 17:26:51 +0300

    test: add new test cases

--- __tests__/generate.util.test.ts
+++ __tests__/generate.util.test.ts
@@ -444,5 +444,158 @@ describe("[generate]", () => {
         await expect(generate(nodes, edges, true)).resolves.toBe(expected);
       });
     });
+    describe("✨ flatten ✨ merged ✨", () => {
+      test("resolves two true and two false children merged into one", async () => {
+        const nodes: Node[] = [
+          { id: "n0", type: "start", position: { x: 0, y: 0 }, data: { text: "n0" } },
+          { id: "n1", type: "find", position: { x: 0, y: 0 }, data: { text: "n1" } },
+          { id: "n2", type: "click", position: { x: 0, y: 0 }, data: { text: "n2" } },
+          { id: "n3", type: "click", position: { x: 0, y: 0 }, data: { text: "n3" } },
+          { id: "n4", type: "click", position: { x: 0, y: 0 }, data: { text: "n4" } },
+          { id: "n5", type: "click", position: { x: 0, y: 0 }, data: { text: "n5" } },
+          { id: "n6", type: "click", position: { x: 0, y: 0 }, data: { text: "n6" } },
+        ];
+        const edges: Edge[] = [
+          { id: "e1", source: "n0", target: "n1" },
+          { id: "e2", source: "n1", target: "n2", sourceHandle: "true" },
+          { id: "e3", source: "n2", target: "n3" },
+          { id: "e4", source: "n1", target: "n4", sourceHandle: "false" },
+          { id: "e5", source: "n4", target: "n5" },
+          { id: "e6", source: "n5", target: "n6" },
+          { id: "e7", source: "n3", target: "n6" },
+        ];
+
+        let expected = "";
+        expected += getTryExceptCode("try", 1);
+        expected += getBlockCode(nodes[1], 2);
+        expected += getBlockCode(nodes[2], 2);
+        expected += getBlockCode(nodes[3], 2);
+        expected += getTryExceptCode("except", 1);
+        expected += getBlockCode(nodes[4], 2);
+        expected += getBlockCode(nodes[5], 2);
+        expected += getBlockCode(nodes[6], 1);
+        expected += "\n";
+        await expect(generate(nodes, edges, true)).resolves.toBe(expected);
+      });
+
+      test("resolves two true and no false children merged into one", async () => {
+        const nodes: Node[] = [
+          { id: "n0", type: "start", position: { x: 0, y: 0 }, data: { text: "n0" } },
+          { id: "n1", type: "find", position: { x: 0, y: 0 }, data: { text: "n1" } },
+          { id: "n2", type: "click", position: { x: 0, y: 0 }, data: { text: "n2" } },
+          { id: "n3", type: "click", position: { x: 0, y: 0 }, data: { text: "n3" } },
+          { id: "n4", type: "click", position: { x: 0, y: 0 }, data: { text: "n4" } },
+        ];
+        const edges: Edge[] = [
+          { id: "e1", source: "n0", target: "n1" },
+          { id: "e2", source: "n1", target: "n2", sourceHandle: "true" },
+          { id: "e3", source: "n2", target: "n3" },
+          { id: "e4", source: "n1", target: "n4", sourceHandle: "false" },
+          { id: "e7", source: "n3", target: "n4" },
+        ];
+
+        let expected = "";
+        expected += getTryExceptCode("try", 1);
+        expected += getBlockCode(nodes[1], 2);
+        expected += getBlockCode(nodes[2], 2);
+        expected += getBlockCode(nodes[3], 2);
+        expected += getTryExceptCode("except", 1);
+        expected += getBlockCode(nodes[4], 1);
+        expected += "\n";
+        await expect(generate(nodes, edges, true)).resolves.toBe(expected);
+      });
+
+      test("resolves no true and two false children merged into one", async () => {
+        const nodes: Node[] = [
+          { id: "n0", type: "start", position: { x: 0, y: 0 }, data: { text: "n0" } },
+          { id: "n1", type: "find", position: { x: 0, y: 0 }, data: { text: "n1" } },
+          { id: "n2", type: "click", position: { x: 0, y: 0 }, data: { text: "n2" } },
+          { id: "n3", type: "click", position: { x: 0, y: 0 }, data: { text: "n3" } },
+          { id: "n4", type: "click", position: { x: 0, y: 0 }, data: { text: "n4" } },
+        ];
+        const edges: Edge[] = [
+          { id: "e1", source: "n0", target: "n1" },
+          { id: "e2", source: "n1", target: "n2", sourceHandle: "false" },
+          { id: "e3", source: "n2", target: "n3" },
+          { id: "e4", source: "n1", target: "n4", sourceHandle: "true" },
+          { id: "e7", source: "n3", target: "n4" },
+        ];
+
+        let expected = "";
+        expected += getTryExceptCode("try", 1);
+        expected += getBlockCode(nodes[1], 2);
+        expected += getTryExceptCode("except", 1);
+        expected += getBlockCode(nodes[2], 2);
+        expected += getBlockCode(nodes[3], 2);
+        expected += getBlockCode(nodes[4], 1);
+        expected += "\n";
+        await expect(generate(nodes, edges, true)).resolves.toBe(expected);
+      });
+
+      test("resolves no true and no false children merged into one", async () => {
+        const nodes: Node[] = [
+          { id: "n0", type: "start", position: { x: 0, y: 0 }, data: { text: "n0" } },
+          { id: "n1", type: "find", position: { x: 0, y: 0 }, data: { text: "n1" } },
+          { id: "n2", type: "click", position: { x: 0, y: 0 }, data: { text: "n2" } },
+          { id: "n3", type: "click", position: { x: 0, y: 0 }, data: { text: "n3" } },
+        ];
+        const edges: Edge[] = [
+          { id: "e1", source: "n0", target: "n1" },
+          { id: "e2", source: "n1", target: "n2", sourceHandle: "false" },
+          { id: "e3", source: "n2", target: "n3" },
+          { id: "e4", source: "n1", target: "n2", sourceHandle: "true" },
+        ];
+
+        let expected = "";
+        expected += getTryExceptCode("try", 1);
+        expected += getBlockCode(nodes[1], 2);
+        expected += getTryExceptCode("except", 1);
+        expected += getBlockCode(nodes[2], 1);
+        expected += getBlockCode(nodes[3], 1);
+        expected += "\n";
+        await expect(generate(nodes, edges, true)).resolves.toBe(expected);
+      });
+
+      test("resolves two find blocks with merged false handle", async () => {
+        const nodes: Node[] = [
+          { id: "n0", type: "start", position: { x: 0, y: 0 }, data: { text: "n0" } },
+          { id: "n1", type: "find", position: { x: 0, y: 0 }, data: { text: "n1" } },
+          { id: "n2", type: "find", position: { x: 0, y: 0 }, data: { text: "n2" } },
+          { id: "n3", type: "click", position: { x: 0, y: 0 }, data: { text: "n3" } },
+          { id: "n4", type: "click", position: { x: 0, y: 0 }, data: { text: "n4" } },
+          { id: "n5", type: "click", position: { x: 0, y: 0 }, data: { text: "n5" } },
+          { id: "n6", type: "click", position: { x: 0, y: 0 }, data: { text: "n6" } },
+        ];
+        const edges: Edge[] = [
+          { id: "e1", source: "n0", target: "n1" },
+          { id: "e2", source: "n0", target: "n2" },
+          { id: "e3", source: "n1", target: "n3", sourceHandle: "false" },
+          { id: "e4", source: "n2", target: "n3", sourceHandle: "false" },
+          { id: "e5", source: "n1", target: "n4", sourceHandle: "true" },
+          { id: "e6", source: "n2", target: "n5", sourceHandle: "true" },
+          { id: "e7", source: "n3", target: "n6" },
+        ];
+
+        let expected = "";
+        expected += getTryExceptCode("try", 1);
+        expected += getBlockCode(nodes[1], 2);
+        expected += getBlockCode(nodes[4], 2);
+        expected += getTryExceptCode("except", 1);
+        expected += getBlockCode(nodes[3], 2);
+        expected += getBlockCode(nodes[6], 2);
+        expected += "\n";
+        expected += getTryExceptCode("try", 1);
+        expected += getBlockCode(nodes[2], 2);
+        expected += getBlockCode(nodes[5], 2);
+        expected += getTryExceptCode("except", 1);
+        expected += getBlockCode(nodes[3], 2);
+        expected += getBlockCode(nodes[6], 2);
+        expected += "\n";
+        await expect(generate(nodes, edges, true)).resolves.toBe(expected);
+      });
+    });
+    describe("✨ nested ✨ merged ✨", () => {
+      // TODO: add test cases
+    });
   });
 });
