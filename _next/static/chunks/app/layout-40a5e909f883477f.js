a502d7d21fd9f508c1fb42b65264fcb021653dc0
spaceymonk <b.kaan.ozkan@gmail.com>
2023-07-16 21:22:11 +0300

    deploy

---
+++ _next/static/chunks/app/layout-40a5e909f883477f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{6117:function(n,e,u){Promise.resolve().then(u.t.bind(u,7402,23))},7402:function(){}},function(n){n.O(0,[253,769,744],function(){return n(n.s=6117)}),_N_E=n.O()}]);
